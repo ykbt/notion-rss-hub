5114a06ca715ab60cafb30f4f7037d794280174f
Yusuke Kubota <ykubota.rec@gmail.com>
2023-09-07 11:38:28 +0900

    テスト

--- src/RssArticle.test.ts
+++ src/RssArticle.test.ts
@@ -6,17 +6,15 @@ describe('RssArticle', () => {
     describe('正常系', () => {
       test.todo('RSSのURLにリクエストした結果が空の場合、空配列が返る');
       test.todo('RSSのURLにリクエストした結果が1つ以上存在する場合、Articleの配列が返る');
+      test('urlが空文字の場合空配列が返る', async () => {
+        const rssArticle = new RssArticle();
+        const feedSource = { Name: '', Url: '' };
+        const result = await rssArticle.fetchArticles(feedSource);
+        expect(result).toEqual([]);
+      });
     });
 
     describe('異常系', () => {
-      test('urlが空文字の場合エラーが返る', async () => {
-        const rssArticle = new RssArticle();
-        const feedSource = { Name: '', Url: '' };
-        await expect(rssArticle.fetchArticles(feedSource)).rejects.toThrowError(
-          new Error('connect ECONNREFUSED 127.0.0.1:80')
-        );
-      });
-
       test.todo('urlが不正な値の場合エラーが返る');
       test.todo('URLへのリクエストに失敗した場合、hogehoge');
     });
